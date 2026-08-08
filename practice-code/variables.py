c7460e2251757927dd54e964bf50fe56a7ac69d7
amixiid <amixiid0@gmail.com>
2026-08-08 11:52:24 +0300

    add variable practice file

---
+++ practice-code/variables.py
@@ -0,0 +1,43 @@
+name = "amixiid"
+age = 20
+country = "somaliland"
+
+age = 30
+
+age = 50
+print(name)
+print(age)
+print(country)
+
+# string variable
+language = "python"
+print(language)
+
+# int variable
+age = 25
+print(age)
+
+# float variable
+height = 1.75
+print(height)
+
+# boolean variable
+is_student = False
+print(is_student)
+
+
+name = "aragsoft"
+print(type(name))
+
+
+# name = input("Enter your name: ")
+# print("your name is " ,name)
+
+# name = int(input("Enter your age: "))
+# print("your age is " ,name)
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("your name is " ,name)
+print("your age is " ,age)
